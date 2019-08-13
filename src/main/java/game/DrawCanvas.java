8aa1cd2d7c49a87d7621f779c2515b2c81c4a977
DieDreiSchwalben <github@woernlein.com>
2019-08-13 14:14:44 +0200

    First commit of this branch

---
+++ src/main/java/game/DrawCanvas.java
@@ -0,0 +1,17 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawCanvas extends JPanel {
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        setBackground(Color.BLACK);
+        g.setColor(Color.YELLOW);    // set the drawing color
+        g.drawLine(30, 40, 100, 200);
+
+    }
+}
