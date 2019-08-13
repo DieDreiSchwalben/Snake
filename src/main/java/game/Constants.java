58da58b70a37ecd731ee10f0e8738e54076af894
DieDreiSchwalben <github@woernlein.com>
2019-08-13 13:58:13 +0200

    Commit before creating feature branch

--- src/main/java/game/Constants.java
+++ src/main/java/game/Constants.java
@@ -8,7 +8,6 @@ public interface Constants {
     public static final int COLS = 10;
     public static final int ROWS = 10;
 
-
     public static final int PIXEL_WIDTH = CANVAS_WIDTH / COLS;
     public static final int PIXEL_HEIGHT = CANVAS_HEIGHT / ROWS;
 
