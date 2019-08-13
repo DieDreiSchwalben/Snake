1c33fcbc32f63559d0cfd7b0c7551abec1225f11
DieDreiSchwalben <github@woernlein.com>
2019-08-13 13:31:40 +0200

    Stupid problems with pull - so i merge

--- src/main/java/game/Constants.java
+++ src/main/java/game/Constants.java
@@ -8,6 +8,7 @@ public interface Constants {
     public static final int COLS = 10;
     public static final int ROWS = 10;
 
+
     public static final int PIXEL_WIDTH = CANVAS_WIDTH / COLS;
     public static final int PIXEL_HEIGHT = CANVAS_HEIGHT / ROWS;
 
