4d0e0fa0a8c126f498396ecc2bf9da04f7038859
DieDreiSchwalben <github@woernlein.com>
2019-08-15 10:04:08 +0200

    Added Arrow key-listening-switch

--- src/main/java/game/SnakeGame.java
+++ src/main/java/game/SnakeGame.java
@@ -1,15 +1,30 @@
 package game;
 
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class SnakeGame extends JFrame implements KeyListener, Constants {
+public class SnakeGame extends JFrame implements KeyListener {
+
+    public static final int WINDOW_BAR_HEIGHT = 20;
+
+    public static final int CANVAS_WIDTH = 400; // pixel
+    public static final int CANVAS_HEIGHT = 400; // pixel
+    public static final int DELAY = 10; // milliseconds
+
+    public static final int COLS = 10; // number of columns
+    public static final int ROWS = 10; // number of rows
+
+    public static final int COLUMN_WIDTH = CANVAS_WIDTH / COLS; // width of columns
+    public static final int ROW_HEIGHT = CANVAS_HEIGHT / ROWS; // height of rows
+
+    public static final Color BACKGROUND_COLOR = Color.WHITE;
+    public static final Color DEFAULT_STROKE_COLOR = Color.BLACK;
+    public static final Color DEFAULT_FILLED_COLOR = Color.GREEN;
 
     private Color nextColor = Color.BLACK;
 
@@ -66,6 +81,27 @@ public class SnakeGame extends JFrame implements KeyListener, Constants {
     }
 
     public void keyPressed(KeyEvent event) {
+
+        int keyCode = event.getKeyCode();
+        switch( keyCode ) {
+            case KeyEvent.VK_UP:
+                // handle up
+                System.out.println("up");
+                break;
+            case KeyEvent.VK_DOWN:
+                // handle down
+                System.out.println("up");
+                break;
+            case KeyEvent.VK_LEFT:
+                // handle left
+                System.out.println("up");
+                break;
+            case KeyEvent.VK_RIGHT :
+                // handle right
+                System.out.println("up");
+                break;
+        }
+
 
         switch(event.getKeyChar())  {
             case 's':
